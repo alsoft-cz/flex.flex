51765608efd707ab416b9759aab805d42c902a25
petrk <mormegil@centrum.cz>
2004-09-24 13:30:04 +0000

    Import 4.0.5.3

--- Bin/Compiler/release_msi.cmd
+++ Bin/Compiler/release_msi.cmd
@@ -39,8 +39,6 @@ echo.
 echo ----- Building MSI ---------------------------------------------------
 echo.
 
-rem %%TODO "lite" verze, bez PHP a dokumentace (getting started ano)
-
 mkdir release
 mkdir release\msi
 del /q release\msi\*.*
@@ -55,6 +53,20 @@ if errorlevel 1 goto err_abort
 
 copy "..\..\Tools\Setup\modFlex 4.0 %2 Edition\modFlex\modFlex Release\modFlex Release %1\DiskImages\Disk1\modFlex.msi" release\msi\modflex.msi
 if errorlevel 1 goto err_abort
+
+
+del "..\..\Tools\Setup\modFlex 4.0 %2 Edition\modflex-lite.ism"
+copy "..\..\Tools\Setup\modFlex 4.0 %2 Edition\modflex-lite_part1.ism" "..\..\Tools\Setup\modFlex 4.0 %2 Edition\modflex-lite.ism"
+echo %1^</td^>^</row^> >>  "..\..\Tools\Setup\modFlex 4.0 %2 Edition\modflex-lite.ism"
+type  "..\..\Tools\Setup\modFlex 4.0 %2 Edition\modflex-lite_part2.ism" >>  "..\..\Tools\Setup\modFlex 4.0 %2 Edition\modflex-lite.ism"
+
+"C:\Program Files\InstallShield\DevStudio 9\System\iscmdbld" -p "..\..\Tools\Setup\modFlex 4.0 %2 Edition\modflex-lite.ism" -r "modFlex Release %1" -c COMP -a "modFlex Release" -e n
+if errorlevel 1 goto err_abort
+
+copy "..\..\Tools\Setup\modFlex 4.0 %2 Edition\modFlex-lite\modFlex Release\modFlex Release %1\DiskImages\Disk1\modFlex.msi" release\msi\modflex-lite.msi
+if errorlevel 1 goto err_abort
+
+
 goto exit
 
 :err_abort
